4ad9dc3204266e0cfc9049d5529c082694e9652d
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2021-05-10 09:05:30 +0200

    a

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let onLoadFunc = function f() {
+let buttons
 
+let onLoadFunc = function f() {
+    buttons = document.querySelectorAll('[data-toggle="dropdown"]')
+    buttons.forEach(btn => {
+        if(btn.dataset.target) {
+            let menu = document.querySelector(btn.dataset.target)
+            let c = btn.getBoundingClientRect()
+            menu.style.display = 'none'
+            menu.style.top = `${c.top+btn.offsetHeight}px`
+            menu.style.left = `${c.left}px`
+            menu.style.width = `${btn.offsetWidth + 20}px`
+            menu.style.position = 'absolute'
+            btn.addEventListener('click', () => {
+                menu.style.display = menu.style.display === 'none' ? 'block' : 'none'
+            })
+        }
+    })
 }
 
 window.onload = onLoadFunc
